ce7d3836a6f9d3e38dc083ce7477829a5beea1dd
Xiyang Liu <yippeecai@gmail.com>
2019-06-18 20:10:09 -0700

    Updates

---
+++ precache-manifest.c9600cd7a8597bffe7b9365c04ee4d12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ee83ac4f9f52292e6cdd2b4a747071",
+    "url": "/ej2-react-demo/index.html"
+  },
+  {
+    "revision": "7d2cf99e17cdea0337b8",
+    "url": "/ej2-react-demo/static/css/main.75565b45.chunk.css"
+  },
+  {
+    "revision": "585beba26e4e0fa71511",
+    "url": "/ej2-react-demo/static/js/2.da2882d8.chunk.js"
+  },
+  {
+    "revision": "7d2cf99e17cdea0337b8",
+    "url": "/ej2-react-demo/static/js/main.e77684b9.chunk.js"
+  },
+  {
+    "revision": "228e3829d07b85acface",
+    "url": "/ej2-react-demo/static/js/runtime~main.9c2d7f25.js"
+  }
+]);
